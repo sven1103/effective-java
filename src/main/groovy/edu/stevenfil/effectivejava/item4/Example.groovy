feaf9bec7f35fea51c68dd7580469e7d1c6ff0af
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-03-22 20:54:43 +0100

    Provides example for item 4

---
+++ src/main/groovy/edu/stevenfil/effectivejava/item4/Example.groovy
@@ -0,0 +1,14 @@
+package edu.stevenfil.effectivejava.item4
+
+/**
+ * Item 4: Noninstantiability
+ *
+ * @author: Sven Fillinger
+ */
+class Example {
+
+    static void main(String[] args) {
+        println MathUtils.plus(2, 4)
+    }
+
+}
---
+++ src/main/groovy/edu/stevenfil/effectivejava/item4/MathUtils.groovy
@@ -0,0 +1,19 @@
+package edu.stevenfil.effectivejava.item4
+
+/**
+ * <add class description here>
+ *
+ * @author: Sven Fillinger
+ */
+class MathUtils {
+
+
+    static int plus(int a, int b) {
+        return a + b
+    }
+
+    static int minus(int a, int b) {
+        return a - b
+    }
+
+}
